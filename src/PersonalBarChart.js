52148c756a8637cf00b67d57a87016ae0c7d42ef
MiniloLY <simliangyan@gmail.com>
2021-03-27 12:00:16 +0800

    ignore

--- src/PersonalBarChart.js
+++ src/PersonalBarChart.js
@@ -1,6 +1,6 @@
 import {Bar} from 'vue-chartjs'
 //import {database} from '../firebase.js'
-//import {EventPassing} from '../passingid.js'
+//import {EventPassing} from '../passingid.js' //use this to get doc_id
 
 
 export default {
@@ -36,24 +36,24 @@ export default {
   },
   methods: {
     fetchItems: function () {
-      /*var totalSum = {
-        "Listings posted": 0,
-        "Products sold": 0
-      }
+        /*var totalSum = {
+            "Listings posted": 0,
+            "Products sold": 0
+        }
 
-      database.collection('user').document('this.doc_id').then(querySnapShot => {
-        
-          //var data = querySnapShot.data()
-          //totalSum["Listings posted"] = data.productsListed
-          
+        database.collection('user').document('this.doc_id').then(querySnapShot => {
+            var listedData = querySnapShot.get('productsListed')
+            var soldData = querySnapShot.get('productsSold')
+            totalSum["Listings posted"] = listedData.length  
+            totalSum["Products sold"] = soldData.length  
         })
 
         for (var info in totalSum) {
-          this.datacollection.datasets[0].data.push(totalSum[info])
-          this.datacollection.labels.push(info)
-        }
+            this.datacollection.datasets[0].data.push(totalSum[info])
+            this.datacollection.labels.push(info)
+        }*/
 
-        this.renderChart(this.datacollection, this.options)*/
+        this.renderChart(this.datacollection, this.options)
       }
   },
   created() {
--- src/components/PersonalDashboard.vue
+++ src/components/PersonalDashboard.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
-        <bar-chart></bar-chart>
+        
     </div>
 </template>
 
 <script>
-import BarChart from "../PersonalBarChart.vue";
+/*import BarChart from "../PersonalBarChart.vue";
 
 export default {
     components: {
         BarChart
     }
-}
+}*/
 </script>
 
 <style scoped>
