970218129643328c4d9bab8a6243e3df44372355
sharleneq <65596820+sharleneq@users.noreply.github.com>
2021-03-31 00:57:13 +0800

    added input fields, link firestore: product & user

--- src/components/ImgInput.vue
+++ src/components/ImgInput.vue
@@ -1,43 +1,50 @@
 <template>
-<div
+  <div
     class="imgArea"
     v-bind:style="{ 'background-image': `url(${imageData})` }"
     v-on:click="insertImg"
->
-
+  >
     <span v-if="!imageData" class="placeholder">
-        + <br> Upload an Image
+      + <br />
+      Upload an Image
     </span>
-    <input class="picInput" ref="fileInput" type="file" v-on:input="onSelectFile" v-on:change="onSelectFile" accept="image/*" required>
-</div>
-
+    <input
+      class="picInput"
+      ref="fileInput"
+      type="file"
+      v-on:input="onSelectFile"
+      v-on:change="onSelectFile"
+      accept="image/*"
+      required
+    />
+  </div>
 </template>
 
 <script>
 export default {
-    data () {
-          return {
-              imageData: null
-          }
+  data() {
+    return {
+      imageData: null,
+    };
+  },
+  methods: {
+    insertImg() {
+      this.$refs.fileInput.click();
     },
-    methods: {
-        insertImg() {
-            this.$refs.fileInput.click()
-        },
-        onSelectFile() {
-            const input = this.$refs.fileInput
-            const files = input.files
-            if (files && files[0]) {
-                const reader = new FileReader
-                reader.onload = e => {
-                    this.imageData = e.target.result
-                }
-                reader.readAsDataURL(files[0])
-                this.$emit('input', files[0])
-            }
-        }
-    }
-}
+    onSelectFile() {
+      const input = this.$refs.fileInput;
+      const files = input.files;
+      if (files && files[0]) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.imageData = e.target.result;
+        };
+        reader.readAsDataURL(files[0]);
+        this.$emit("input", files[0]);
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
