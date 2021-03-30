b519496cb05a246c8cdf5e585270d530c77920ef
sharleneq <65596820+sharleneq@users.noreply.github.com>
2021-03-31 01:37:55 +0800

    added news and modified styling

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,80 +1,109 @@
 <template>
   <section id="news">
-        <h1 id="head">News Page</h1>
+    <h1 id="head">News Page</h1>
     <div>
-        <ul>
-          <li v-for='(item, id) in itemsList' :key='id'>
-            <div class="polaroid">
-              <div class="fill"><img v-bind:src="item.imageURL"/></div>
-              <div class="container"><a v-bind:href="item.url" id="url">{{item.name}}</a></div>
+      <ul>
+        <li v-for="(item, id) in itemsList" :key="id">
+          <div class="polaroid">
+            <div class="fill"><img v-bind:src="item.imageURL" /></div>
+            <div class="container">
+              <a v-bind:href="item.url" id="url">{{ item.name }}</a>
             </div>
-          </li>
+          </div>
+        </li>
       </ul>
-      </div>
-    <div id= "communityDashboard">
+    </div>
+    <div id="communityDashboard">
       <CommunityDashboard></CommunityDashboard>
     </div>
   </section>
 </template>
 
 <script>
-import CommunityDashboard from './CommunityDashboard.vue'
+import CommunityDashboard from "./CommunityDashboard.vue";
 
 export default {
-    components: {
-      CommunityDashboard
-    },
-    data(){
-        return{
+  components: {
+    CommunityDashboard,
+  },
+  data() {
+    return {
       itemsList: [
         {
           id: "#000",
           name: "Why are fashion brands investing in garment recycling?",
-          url: "https://www.greenbiz.com/article/fashions-latest-trend-why-hm-other-big-brands-are-investing-garment-recycling",
-          imageURL:"https://i2.wp.com/www.prettylittlelondon.com/wp-content/uploads/2019/09/hm-Cover-2.jpg",
+          url:
+            "https://www.greenbiz.com/article/fashions-latest-trend-why-hm-other-big-brands-are-investing-garment-recycling",
+          imageURL:
+            "https://i2.wp.com/www.prettylittlelondon.com/wp-content/uploads/2019/09/hm-Cover-2.jpg",
         },
         {
-          id: "#025",
+          id: "#001",
           name: "15 recycling efforts that are reshaping the fashion industry",
-          url: "https://fashionunited.uk/news/business/15-recycling-efforts-that-are-reshaping-the-fashion-industry/2019051443162",
+          url:
+            "https://fashionunited.uk/news/business/15-recycling-efforts-that-are-reshaping-the-fashion-industry/2019051443162",
           imageURL:
             "https://fashionunited.uk/qxKL6QbmjaUWI231hn_zrP1hEsiTEdvvDDJK7_9JVYo/gravity:sm/quality:70/aHR0cHM6Ly9zdGF0aWMuZmFzaGlvbnVuaXRlZC5jb20vMjAxOTA1L1JlY3ljbGluZzIuanBn",
         },
         {
-          id: "#067",
+          id: "#002",
           name: "Textiles Wastage In Singapore...  What can we do?",
           url: "https://blogs.ntu.edu.sg/hp3203-2018-27/singapore/",
-          imageURL: "https://eco-business.imgix.net/uploads/ebmedia/fileuploads/unnamed-2.jpg?fit=crop&h=960&ixlib=django-1.2.0&w=1440",
+          imageURL:
+            "https://eco-business.imgix.net/uploads/ebmedia/fileuploads/unnamed-2.jpg?fit=crop&h=960&ixlib=django-1.2.0&w=1440",
         },
         {
-          id: "#077",
-          name: "Singapore swapping initiatives offer alternative to fast fashion",
-          url: "https://fashionunited.uk/news/fashion/singapore-swapping-initiatives-offer-alternative-to-fast-fashion/2021021553581",
+          id: "#003",
+          name:
+            "Singapore swapping initiatives offer alternative to fast fashion",
+          url:
+            "https://fashionunited.uk/news/fashion/singapore-swapping-initiatives-offer-alternative-to-fast-fashion/2021021553581",
           imageURL:
             "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iZe_k2zn.2dQ/v0/3000x-1.jpg",
         },
         {
-          id: "#099",
-          name: "Fast fashion, fast trash: Industry needs to arrest carbon impact, find sustainable solutions",
-          url: "https://www.todayonline.com/singapore/fast-fashion-fast-trash-industry-needs-arrest-carbon-impact-find-sustainable-solutions",
+          id: "#004",
+          name:
+            "Fast fashion, fast trash: Industry needs to arrest carbon impact, find sustainable solutions",
+          url:
+            "https://www.todayonline.com/singapore/fast-fashion-fast-trash-industry-needs-arrest-carbon-impact-find-sustainable-solutions",
           imageURL:
             "https://www.todayonline.com/sites/default/files/styles/new_app_article_detail/public/photos/43_images/waldemar-brandt-1408730-unsplash.jpg?itok=3r7extUp",
         },
-      ]        }
-    },
-    methods:{
-    }
-}
+        {
+          id: "#005",
+          name:
+            "New forces are reinventing fashion and bringing change to the industry",
+          url:
+            "https://www.straitstimes.com/life/style/new-forces-are-reinventing-fashion-and-bringing-change-to-the-industry",
+          imageURL:
+            "https://static.straitstimes.com.sg/s3fs-public/styles/article_pictrure_780x520_/public/articles/2020/12/31/af_marineserre_311220.jpg?itok=46_kJgwo&timestamp=1609394420",
+        },
+        {
+          id: "#006",
+          name:
+            "Commentary: Taking Singapore’s green ambitions to new heights with a circular economy",
+          url:
+            "https://www.channelnewsasia.com/news/commentary/singapore-circular-economy-sustainability-community-climate-14367536",
+          imageURL:
+            "https://cna-sg-res.cloudinary.com/image/upload/q_auto,f_auto/image/10599402/16x9/991/557/63325bb76cb1794e6ef64839c158a4f/TI/clothes-on-racks-in-a-thrift-shop.jpg",
+        },
+
+      ],
+    };
+  },
+  methods: {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #news {
-  height: 100vh;
+  height: 275vh;
 }
 .container a {
-    color: aliceblue;
-    font-weight: 800;
+  color: aliceblue;
+  font-weight: 800;
   padding: 16px;
   text-decoration: none;
   display: block;
@@ -102,9 +131,9 @@ li {
 
 img {
   height: 170px;
-    flex-shrink: 0;
-    min-width: 100%;
-    min-height: 100%
+  flex-shrink: 0;
+  min-width: 100%;
+  min-height: 100%;
 }
 
 div.polaroid {
@@ -117,5 +146,4 @@ div.polaroid {
 div.container {
   padding: 10px;
 }
-
 </style>
