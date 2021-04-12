8bf03cd2f506f8cfb5089b2fbd8f6e28cd0d11a4
sharleneq <65596820+sharleneq@users.noreply.github.com>
2021-04-12 14:48:00 +0800

    updated colors to display in chrome

--- src/CommunityLineChart.js
+++ src/CommunityLineChart.js
@@ -8,7 +8,7 @@ export default {
       datacollection: {
         labels: [],
         datasets: [{
-          backgroundColor: "006d77",
+          backgroundColor: "#006d77",
           data: [],
           fill: false
         }]
