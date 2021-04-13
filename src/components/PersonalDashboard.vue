24a56dc3250189cd6223f9d0f2187969fe241d0e
sharleneq <65596820+sharleneq@users.noreply.github.com>
2021-04-13 19:13:06 +0800

    minor edits for styling

--- src/components/PersonalDashboard.vue
+++ src/components/PersonalDashboard.vue
@@ -2,8 +2,10 @@
   <section>
     <NavBar></NavBar>
     <h2 class="title">PERSONAL DASHBOARD</h2>
+    <br />
     <h3>Total Number of products listed/sold</h3>
     <BarChart></BarChart>
+    <br />
     <br />
     <h3>Number of products in various categories</h3>
     <PieChart></PieChart>
@@ -26,6 +28,10 @@ export default {
 .title {
   border-bottom: 1px solid black;
   padding-bottom: 50px;
+  font-size: 25px;
+}
+
+h3 {
   font-size: 25px;
 }
 </style>
